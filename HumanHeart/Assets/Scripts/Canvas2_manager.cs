3a7d4f199bc513fc7a7fa2d47ffdfafe3c7c8e35
Kunal Panigrahi <75083810+Kunal027@users.noreply.github.com>
2021-04-26 12:49:04 +0530

    learning scene uppgrade
    1. learning scene 1 with normal ui
    2. learning scene 2 with the world space canvas UI

---
+++ HumanHeart/Assets/Scripts/Canvas2_manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas2_manager : MonoBehaviour
+{
+
+    public GameObject world_Space_Canvas;
+
+    public GameObject aboutButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void deactivate_about_button()
+    {
+        aboutButton.SetActive(false);
+    }
+    public void deactivate_WorldSpace_Canvas()
+    {
+        world_Space_Canvas.SetActive(false);
+    }
+}
---
+++ HumanHeart/Assets/Scripts/Indicator_manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+
+public class Indicator_manager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject aorta_indicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Aourta_buttonClicked()
+    {
+        StartCoroutine(Aourta_Indicator_Coroutine());
+    }
+  
+
+    IEnumerator Aourta_Indicator_Coroutine()
+    {
+        aorta_indicator.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        aorta_indicator.SetActive(false);
+
+    }
+}
