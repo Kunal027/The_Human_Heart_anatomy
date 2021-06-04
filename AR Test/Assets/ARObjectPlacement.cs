9592a9e1ba72eed4c04862e87166c6a0a0e5ca77
Kunal Panigrahi <kunalpanigrahi@hotmail.com>
2021-06-04 19:15:20 +0530

    cache

---
+++ AR Test/Assets/ARObjectPlacement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARObjectPlacement : MonoBehaviour
+{
+
+    public ARSessionOrigin ar_sessionOrigin;
+    public List<ARRaycastHit> arRaycastHits = new List<ARRaycastHit>();
+    public GameObject heart;
+
+    GameObject instantiatedModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //detecting the user touch
+        //projecting a raycast
+        //instantiate the model
+
+        if(Input.GetMouseButton(0))
+        {
+            bool col = ar_sessionOrigin.GetComponent<ARRaycastManager>().Raycast(Input.mousePosition, arRaycastHits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon);
+            if(col)
+            {
+                if (instantiatedModel == null)
+                {
+                    Instantiate(heart);
+
+                    foreach (var plane in ar_sessionOrigin.GetComponent<ARPlaneManager>().trackables)
+                    {
+                        plane.gameObject.SetActive(false);
+                    }
+                    ar_sessionOrigin.GetComponent<ARPlaneManager>().enabled = false;
+                }
+                instantiatedModel.transform.position = arRaycastHits[0].pose.position;
+            }
+        }
+    }
+}
--- HumanHeart/Assets/Scripts/SceneManager.cs
+++ HumanHeart/Assets/Scripts/SceneManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-
+    public GameObject selectionPanel;
     void Start()
     {
         
@@ -19,7 +19,7 @@ public class SceneManager : MonoBehaviour
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
-    public void load_Learning_scene()
+    public void load_Learning_scene_external()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
@@ -33,6 +33,17 @@ public class SceneManager : MonoBehaviour
     public void QuitApplication()
     {
         Application.Quit();
+    }
+
+
+    public void selectionPanelActive()
+    {
+        selectionPanel.SetActive(true);
+    }
+
+    public void selectionPanelDeactivate()
+    {
+        selectionPanel.SetActive(false);
     }
 }
 
